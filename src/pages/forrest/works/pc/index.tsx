a2f22fa51af84264e18a661e322dda1e4ccd3592
khs <devdev0226@gmail.com>
2025-10-05 21:24:24 +0900

    Feat : 포트폴리오 - 카테고리 애니메이션 추가

--- src/pages/forrest/works/pc/index.tsx
+++ src/pages/forrest/works/pc/index.tsx
@@ -1,13 +1,37 @@
+import { useReducer } from "react";
+
 //component
 import Footer from "@/components/footer";
 import Header from "@components/header";
 
 //resource
+import Arrow from "@/assets/arrow_select.svg?react";
 import sns1 from "@/assets/forrest/sns_1.png";
 import sns2 from "@/assets/forrest/sns_2.png";
 import sns3 from "@/assets/forrest/sns_3.png";
 
+const initialState = {
+  open1: false,
+  open2: false,
+  open3: false,
+};
+
+type State = typeof initialState;
+type Action = { type: "TOGGLE"; key: keyof State } | { type: "CLOSE_ALL" };
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "TOGGLE":
+      return { ...state, [action.key]: !state[action.key] };
+
+    default:
+      return state;
+  }
+}
+
 const PcVersion = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
       <Header />
@@ -18,57 +42,62 @@ const PcVersion = () => {
               Category
             </h2>
             <div>
-              <h2
-                style={{
-                  marginTop: "40px",
-                  fontSize: "20px",
-                  fontFamily: "Montserrat-Regular",
-                }}
+              <div
+                className={`category-item ${state.open1 ? "active" : ""}`}
+                onClick={() => dispatch({ type: "TOGGLE", key: "open1" })}
               >
-                Planterior
-              </h2>
-              <ul>
-                <li>All</li>
-                <li>나무1</li>
-                <li>나무2</li>
-              </ul>
+                <h2 className="category-title">Planterior</h2>
+                <span className="arrow">
+                  <Arrow />
+                </span>
+              </div>
+              {state.open1 && (
+                <ul>
+                  <li className="category-item-en">All</li>
+                  <li className="category-item-kr">나무1</li>
+                  <li className="category-item-kr">나무2</li>
+                </ul>
+              )}
             </div>
             <div>
-              <h2
-                style={{
-                  marginTop: "40px",
-                  fontSize: "20px",
-                  fontFamily: "Montserrat-Regular",
-                }}
+              <div
+                className={`category-item ${state.open2 ? "active" : ""}`}
+                onClick={() => dispatch({ type: "TOGGLE", key: "open2" })}
               >
-                gardening
-              </h2>
-              <ul>
-                <li>All</li>
-                <li>나무1</li>
-                <li>나무2</li>
-              </ul>
+                <h2 className="category-title">gardening</h2>
+                <span className="arrow">
+                  <Arrow />
+                </span>
+              </div>
+              {state.open2 && (
+                <ul>
+                  <li className="category-item-en">All</li>
+                  <li className="category-item-kr">나무1</li>
+                  <li className="category-item-kr">나무2</li>
+                </ul>
+              )}
             </div>
-
             <div>
-              <h2
-                style={{
-                  marginTop: "40px",
-                  fontSize: "20px",
-                  fontFamily: "Montserrat-Regular",
-                }}
+              <div
+                className={`category-item ${state.open3 ? "active" : ""}`}
+                onClick={() => dispatch({ type: "TOGGLE", key: "open3" })}
               >
-                artificial plants
-              </h2>
-              <ul>
-                <li>All</li>
-                <li>나무1</li>
-                <li>나무2</li>
-              </ul>
+                <h2 className="category-title">artificial plants</h2>
+                <span className="arrow">
+                  <Arrow />
+                </span>
+              </div>
+              {state.open3 && (
+                <ul>
+                  <li className="category-item-en">All</li>
+                  <li className="category-item-kr">나무1</li>
+                  <li className="category-item-kr">나무2</li>
+                </ul>
+              )}
             </div>
           </div>
         </section>
-        <section className="portfolio">
+        <section className="works-portfolio">
           <div className="works-page-container">
             <div className="works-product-container">
               <div className="works-product-box">
@@ -165,31 +194,11 @@ const PcVersion = () => {
                   </li>
                 </ul>
               </div>
-              <div style={{ display: "flex", justifyContent: "center" }}>
-                <ol style={{ display: "flex", gap: "10px", marginTop: "20px" }}>
-                  <li
-                    style={{
-                      fontWeight: "500",
-                      color: "#bec7aa",
-                      fontFamily: "Montserrat-Regular",
-                    }}
-                  >
-                    1
-                  </li>
-                  <li
-                    style={{
-                      fontFamily: "Montserrat-Regular",
-                    }}
-                  >
-                    2
-                  </li>
-                  <li
-                    style={{
-                      fontFamily: "Montserrat-Regular",
-                    }}
-                  >
-                    3
-                  </li>
+              <div className="works-pagination">
+                <ol className="pagination-list">
+                  <li className="page-number-clicked">1</li>
+                  <li className="page-number">2</li>
+                  <li className="page-number">3</li>
                 </ol>
               </div>
             </div>
