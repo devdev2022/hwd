26d145cb5c49fa46ec81f441020b10758c6b5949
khs <devdev0226@gmail.com>
2025-11-14 19:45:46 +0900

    Fix : config 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react(), svgr()],
@@ -12,9 +11,9 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
+      "@": "/src",
+      "@assets": "/src/assets",
+      "@components": "/src/components",
     },
   },
 });
